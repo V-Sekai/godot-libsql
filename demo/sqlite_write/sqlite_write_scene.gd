735fa1a9858a334589f94ee98e54c19a55801dac
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-10-22 07:54:39 -0700

    Remove WIP triggers.

--- demo/sqlite_write/sqlite_write_scene.gd
+++ demo/sqlite_write/sqlite_write_scene.gd
@@ -54,57 +54,7 @@ CREATE TABLE entity (
 	current_posted	BLOB NOT NULL CHECK( LENGTH(current_posted) <= 1024) DEFAULT (zeroblob(64)),
 	timestamp INTEGER NOT NULL
 ) WITHOUT ROWID, STRICT;
-"""
-#
-#CREATE VIEW entity_view
-#AS 
-#SELECT
-# 	"id", "user_data", "reserved", "shard", "code", "flags", "past_pending", "past_posted",
-#"current_pending", "current_posted", "timestamp"
-#FROM
-#	entity;
-#
-#CREATE VIEW entity_interpolate
-#AS 
-#SELECT
-#	id,
-#	user_data,
-#	reserved,
-#	shard,
-#	code,
-#	flags,
-#	"current_posted" as value
-#FROM
-#	entity;
-#
-#CREATE TRIGGER "entity_interpolate_delete"
-#	INSTEAD OF DELETE ON "entity_interpolate"
-#BEGIN
-#DELETE FROM "entity" WHERE ROWID = OLD.ROWID;
-#END;
-#
-#CREATE TRIGGER "entity_interpolate_update"
-#	INSTEAD OF UPDATE OF "id", "user_data", "reserved", "shard", "code", "flags", "value"
-#	ON "entity_interpolate"
-#BEGIN
-#UPDATE entity
-#   SET "user_data" = old_entity."user_data", "reserved" = old_entity."reserved", "shard" = old_entity."shard", "code" = old_entity."code", "flags" = old_entity."flags", "past_pending" = old_entity.past_pending, "current_pending" = zeroblob(64), "current_posted" = NEW."value", "timestamp" = UNIXEPOCH()
-#  FROM (SELECT "id", "user_data", "reserved", "shard", "code", "flags", "past_pending", "past_posted",
-#"current_pending", "current_posted", "timestamp" FROM entity) AS old_entity
-# WHERE entity.id = NEW.id;
-#END;
-#
-#	var create_trigger = """
-#	CREATE TRIGGER "entity_interpolate_insert"
-#		INSTEAD OF INSERT ON "entity_interpolate"
-#	BEGIN
-#	INSERT INTO "entity"
-#		("id", "user_data", "reserved", "shard", "code", "flags", "past_pending", "past_posted",
-#	"current_pending", "current_posted", "timestamp")
-#	VALUES (NEW."id", NEW."user_data", NEW."reserved", NEW."shard", NEW."code", NEW."flags", zeroblob(64), zeroblob(64), zeroblob(64), NEW."value", UNIXEPOCH());
-#	END;
-#
-	
+"""	
 	var query_create_original = """
 INSERT INTO entity ("id", "user_data", "reserved", "shard", "code", "flags", "past_pending", "past_posted",
 "current_pending", "current_posted", "timestamp")
