41b66d457ea06a365b9c2ba6b145c684d46376b6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-12-02 09:43:58 -0800

    Disable mvsqlite but make it visible.

--- register_types.h
+++ register_types.h
@@ -2,5 +2,5 @@
 
 #include "modules/register_module_types.h"
 
-void initialize_sqlite_module(ModuleInitializationLevel p_level);
-void uninitialize_sqlite_module(ModuleInitializationLevel p_level);
+void initialize_mvsqlite_module(ModuleInitializationLevel p_level);
+void uninitialize_mvsqlite_module(ModuleInitializationLevel p_level);
