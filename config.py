8b2b406b2fd8aaf6195785752e79b96490962895
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-13 14:12:37 -0800

    Remove cross compiling.

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ import platform as python_platform
 def can_build(env, platform):
     if platform == "web":
         return False
+    if platform == "windows":
+        return False
     try:
         subprocess.check_call(["cargo", "--version"])
-        subprocess.check_call(["/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig"])
-        subprocess.check_call(["/usr/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig --libs --static libcrypto"])
     except:
         return False
     return True
