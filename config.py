825cff82b86a498f7bd5eb69f7d9c4205ed65b24
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-13 10:36:06 -0800

    Update config.py and SCsub to add the web

--- config.py
+++ config.py
@@ -4,13 +4,15 @@ import subprocess
 import platform as python_platform
 
 def can_build(env, platform):
+    if env.msvc:
+        return False
     try:
         subprocess.check_call(["cargo", "--version"])
     except:
         return False
     if python_platform.system() == 'Windows' and env["use_mingw"] and env["use_llvm"]:
         return False
-    return not env.msvc
+    return True
 
 def configure(env):
     pass
