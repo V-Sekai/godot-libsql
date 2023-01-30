842bcafd56a6ad5550a6849a7e2ea649d3a76ed7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-30 13:48:19 -0800

    Add libsql_run_wasm prototype.

--- mvsqlite.cpp
+++ mvsqlite.cpp
@@ -7,21 +7,26 @@
 #include "thirdparty/libsql/sqlite3.h"
 
 #define _GNU_SOURCE
+SQLITE_API void libsql_run_wasm(struct libsql_wasm_udf_api *api, sqlite3_context *context,
+                                libsql_wasm_engine_t *engine, libsql_wasm_module_t *module, const char *func_name, int argc, sqlite3_value **argv);
 
-Array libsql_fast_parse_row(sqlite3_stmt *stmt) {
+Array libsql_fast_parse_row(sqlite3_stmt *stmt)
+{
   Array result;
 
   // Get column count
   const int col_count = sqlite3_column_count(stmt);
 
   // Fetch all column
-  for (int i = 0; i < col_count; i++) {
+  for (int i = 0; i < col_count; i++)
+  {
     // Value
     const int col_type = sqlite3_column_type(stmt, i);
     Variant value;
 
     // Get column value
-    switch (col_type) {
+    switch (col_type)
+    {
     case SQLITE_INTEGER:
       value = Variant(sqlite3_column_int(stmt, i));
       break;
@@ -30,14 +35,16 @@ Array libsql_fast_parse_row(sqlite3_stmt *stmt) {
       value = Variant(sqlite3_column_double(stmt, i));
       break;
 
-    case SQLITE_TEXT: {
+    case SQLITE_TEXT:
+    {
       int size = sqlite3_column_bytes(stmt, i);
       String str =
           String::utf8((const char *)sqlite3_column_text(stmt, i), size);
       value = Variant(str);
       break;
     }
-    case SQLITE_BLOB: {
+    case SQLITE_BLOB:
+    {
       PackedByteArray arr;
       int size = sqlite3_column_bytes(stmt, i);
       arr.resize(size);
@@ -45,9 +52,11 @@ Array libsql_fast_parse_row(sqlite3_stmt *stmt) {
       value = Variant(arr);
       break;
     }
-    case SQLITE_NULL: {
+    case SQLITE_NULL:
+    {
       // Nothing to do.
-    } break;
+    }
+    break;
     default:
       ERR_PRINT("This kind of data is not yet supported: " + itos(col_type));
       break;
@@ -63,7 +72,8 @@ LibsqlQuery::LibsqlQuery() {}
 
 LibsqlQuery::~LibsqlQuery() { finalize(); }
 
-void LibsqlQuery::init(Ref<Libsql> p_db, const String &p_query) {
+void LibsqlQuery::init(Ref<Libsql> p_db, const String &p_query)
+{
   db = p_db;
   query = p_query;
   stmt = nullptr;
@@ -71,13 +81,16 @@ void LibsqlQuery::init(Ref<Libsql> p_db, const String &p_query) {
 
 bool LibsqlQuery::is_ready() const { return stmt != nullptr; }
 
-String LibsqlQuery::get_last_error_message() const {
+String LibsqlQuery::get_last_error_message() const
+{
   ERR_FAIL_COND_V(db == nullptr, "Database is undefined.");
   return db->get_last_error_message();
 }
 
-Variant LibsqlQuery::execute(const Array p_args) {
-  if (is_ready() == false) {
+Variant LibsqlQuery::execute(const Array p_args)
+{
+  if (is_ready() == false)
+  {
     ERR_FAIL_COND_V(prepare() == false, Variant());
   }
 
@@ -86,29 +99,37 @@ Variant LibsqlQuery::execute(const Array p_args) {
 
   int autocommit = 0;
   // Error occurred during argument binding
-  if (!Libsql::bind_args(stmt, p_args)) {
+  if (!Libsql::bind_args(stmt, p_args))
+  {
     ERR_FAIL_V_MSG(Variant(),
                    "Error during arguments set: " + get_last_error_message());
   }
   // Execute the query.
   Array result;
-  while (true) {
+  while (true)
+  {
     autocommit = sqlite3_get_autocommit(db->db);
     const int res = sqlite3_step(stmt);
-    if (res == SQLITE_ROW) {
+    if (res == SQLITE_ROW)
+    {
       // Collect the result.
       result.append(libsql_fast_parse_row(stmt));
-    } else if (res == SQLITE_DONE) {
+    }
+    else if (res == SQLITE_DONE)
+    {
       // Nothing more to do.
       break;
-    } else {
+    }
+    else
+    {
       // Error
       ERR_BREAK_MSG(true, "There was an error during an SQL execution: " +
                               get_last_error_message());
     }
   }
 
-  if (SQLITE_OK != sqlite3_reset(stmt)) {
+  if (SQLITE_OK != sqlite3_reset(stmt))
+  {
     finalize();
     ERR_FAIL_V_MSG(result, "Was not possible to reset the query: " +
                                get_last_error_message());
@@ -117,13 +138,16 @@ Variant LibsqlQuery::execute(const Array p_args) {
   return result;
 }
 
-Variant LibsqlQuery::batch_execute(Array p_rows) {
+Variant LibsqlQuery::batch_execute(Array p_rows)
+{
   Array res;
-  for (int i = 0; i < p_rows.size(); i += 1) {
+  for (int i = 0; i < p_rows.size(); i += 1)
+  {
     ERR_FAIL_COND_V_MSG(p_rows[i].get_type() != Variant::ARRAY, Variant(),
                         "An Array of Array is exepected.");
     Variant r = execute(p_rows[i]);
-    if (unlikely(r.get_type() == Variant::NIL)) {
+    if (unlikely(r.get_type() == Variant::NIL))
+    {
       // An error occurred, the error is already logged.
       return Variant();
     }
@@ -132,8 +156,10 @@ Variant LibsqlQuery::batch_execute(Array p_rows) {
   return res;
 }
 
-Array LibsqlQuery::get_columns() {
-  if (is_ready() == false) {
+Array LibsqlQuery::get_columns()
+{
+  if (is_ready() == false)
+  {
     ERR_FAIL_COND_V(prepare() == false, Array());
   }
 
@@ -145,7 +171,8 @@ Array LibsqlQuery::get_columns() {
   res.resize(col_count);
 
   // Fetch all column
-  for (int i = 0; i < col_count; i++) {
+  for (int i = 0; i < col_count; i++)
+  {
     // Key name
     const char *col_name = sqlite3_column_name(stmt, i);
     res[i] = String(col_name);
@@ -154,7 +181,8 @@ Array LibsqlQuery::get_columns() {
   return res;
 }
 
-bool LibsqlQuery::prepare() {
+bool LibsqlQuery::prepare()
+{
   ERR_FAIL_COND_V(stmt != nullptr, false);
   ERR_FAIL_COND_V(db == nullptr, false);
   ERR_FAIL_COND_V(query == "", false);
@@ -170,14 +198,17 @@ bool LibsqlQuery::prepare() {
   return true;
 }
 
-void LibsqlQuery::finalize() {
-  if (stmt) {
+void LibsqlQuery::finalize()
+{
+  if (stmt)
+  {
     sqlite3_finalize(stmt);
     stmt = nullptr;
   }
 }
 
-void LibsqlQuery::_bind_methods() {
+void LibsqlQuery::_bind_methods()
+{
   ClassDB::bind_method(D_METHOD("get_last_error_message"),
                        &LibsqlQuery::get_last_error_message);
   ClassDB::bind_method(D_METHOD("execute", "arguments"),
@@ -187,7 +218,8 @@ void LibsqlQuery::_bind_methods() {
   ClassDB::bind_method(D_METHOD("get_columns"), &LibsqlQuery::get_columns);
 }
 
-Libsql::Libsql() {
+Libsql::Libsql()
+{
   db = nullptr;
 }
 /*
@@ -197,19 +229,23 @@ Libsql::Libsql() {
         @param path The database resource path.
         @return status
 */
-bool Libsql::open(String path) {
-  if (!path.strip_edges().length()) {
+bool Libsql::open(String path)
+{
+  if (!path.strip_edges().length())
+  {
     return false;
   }
   int result = libsql_open(path.utf8().get_data(), &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr, nullptr);
-  if (result != SQLITE_OK) {
+  if (result != SQLITE_OK)
+  {
     print_error("Cannot open the database.");
     sqlite3_close_v2(db);
     db = nullptr;
     return false;
   }
   result = libsql_try_initialize_wasm_func_table(db);
-  if (result != SQLITE_OK) {
+  if (result != SQLITE_OK)
+  {
     print_error("Cannot open the database.");
     sqlite3_close_v2(db);
     db = nullptr;
@@ -218,31 +254,41 @@ bool Libsql::open(String path) {
   return true;
 }
 
-void Libsql::close() {
+void Libsql::close()
+{
   // Finalize all queries before close the DB.
   // Reverse order because I need to remove the not available queries.
-  for (uint32_t i = queries.size(); i > 0; i -= 1) {
+  for (uint32_t i = queries.size(); i > 0; i -= 1)
+  {
     LibsqlQuery *query =
         Object::cast_to<LibsqlQuery>(queries[i - 1]->get_ref());
-    if (query != nullptr) {
+    if (query != nullptr)
+    {
       query->finalize();
-    } else {
+    }
+    else
+    {
       memdelete(queries[i - 1]);
       queries.remove_at(i - 1);
     }
   }
 
-  if (db) {
+  if (db)
+  {
     // Cannot close database!
-    if (sqlite3_close_v2(db) != SQLITE_OK) {
+    if (sqlite3_close_v2(db) != SQLITE_OK)
+    {
       print_error("Cannot close database: " + get_last_error_message());
-    } else {
+    }
+    else
+    {
       db = nullptr;
     }
   }
 }
 
-sqlite3_stmt *Libsql::prepare(const char *query) {
+sqlite3_stmt *Libsql::prepare(const char *query)
+{
   // Get database pointer
   sqlite3 *dbs = get_handler();
 
@@ -259,10 +305,12 @@ sqlite3_stmt *Libsql::prepare(const char *query) {
   return stmt;
 }
 
-bool Libsql::bind_args(sqlite3_stmt *stmt, Array args) {
+bool Libsql::bind_args(sqlite3_stmt *stmt, Array args)
+{
   // Check parameter count
   int param_count = sqlite3_bind_parameter_count(stmt);
-  if (param_count != args.size()) {
+  if (param_count != args.size())
+  {
     print_error("LibsqlQuery failed; expected " + itos(param_count) +
                 " arguments, got " + itos(args.size()));
     return false;
@@ -277,9 +325,11 @@ bool Libsql::bind_args(sqlite3_stmt *stmt, Array args) {
    * - BLOB (1:1 storage)
    */
 
-  for (int i = 0; i < param_count; i++) {
+  for (int i = 0; i < param_count; i++)
+  {
     int retcode;
-    switch (args[i].get_type()) {
+    switch (args[i].get_type())
+    {
     case Variant::Type::NIL:
       retcode = sqlite3_bind_null(stmt, i + 1);
       break;
@@ -307,7 +357,8 @@ bool Libsql::bind_args(sqlite3_stmt *stmt, Array args) {
       return false;
     }
 
-    if (retcode != SQLITE_OK) {
+    if (retcode != SQLITE_OK)
+    {
       print_error(
           "LibsqlQuery failed, an error occured while binding argument" +
           itos(i + 1) + " of " + itos(args.size()) + " (libsql errcode " +
@@ -319,7 +370,8 @@ bool Libsql::bind_args(sqlite3_stmt *stmt, Array args) {
   return true;
 }
 
-Ref<LibsqlQuery> Libsql::create_query(String p_query) {
+Ref<LibsqlQuery> Libsql::create_query(String p_query)
+{
   Ref<LibsqlQuery> query;
   query.instantiate();
   query->init(Ref<Libsql>(this), p_query);
@@ -331,24 +383,29 @@ Ref<LibsqlQuery> Libsql::create_query(String p_query) {
   return query;
 }
 
-String Libsql::get_last_error_message() const {
+String Libsql::get_last_error_message() const
+{
   return sqlite3_errmsg(get_handler());
 }
 
-Libsql::~Libsql() {
+Libsql::~Libsql()
+{
   // Close database
   close();
   // Make sure to invalidate all associated queries.
-  for (uint32_t i = 0; i < queries.size(); i += 1) {
+  for (uint32_t i = 0; i < queries.size(); i += 1)
+  {
     LibsqlQuery *query =
         Object::cast_to<LibsqlQuery>(queries[i]->get_ref());
-    if (query != nullptr) {
+    if (query != nullptr)
+    {
       query->init(nullptr, "");
     }
   }
 }
 
-void Libsql::_bind_methods() {
+void Libsql::_bind_methods()
+{
   ClassDB::bind_method(D_METHOD("get_last_error_message"), &Libsql::get_last_error_message);
   ClassDB::bind_method(D_METHOD("open", "path"), &Libsql::open);
   ClassDB::bind_method(D_METHOD("close"), &Libsql::close);
