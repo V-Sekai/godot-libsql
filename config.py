435f89fa1c876d86c3a5b1500c4df2cb66655913
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-01-30 12:57:22 -0800

    Build offline.

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ def can_build(env, platform):
     if platform == "web":
         return False
     try:
-        subprocess.check_call(["cargo", "--version", "--offline"])
+        subprocess.check_call(["cargo", "--version"])
     except:
         return False
     return True
